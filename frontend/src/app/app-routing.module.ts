6d4095625f5e96c6363f261c586493c157c2a9cc
HendAdel1 <1hend.adel1@gmail.com>
2023-06-22 17:39:37 +0300

    lazy loading in app-routing for home

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
     // lazy loading
     { path:'register', loadChildren: () => import('./register/register.module').then((m) => m.RegisterModule)},
+    { path:'home', loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)},
 ];
 
 @NgModule({
