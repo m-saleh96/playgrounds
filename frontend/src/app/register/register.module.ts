c7ecc64cee694b189eb40e00200f419d79b0a4a9
m-saleh96 <scyllies@gmail.com>
2023-06-22 14:38:25 +0300

    routes login and signup for register module

--- frontend/src/app/register/register.module.ts
+++ frontend/src/app/register/register.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register.component';
 
-
+// routes for register module
+const routes: Routes = [
+  { path:'login', component: LoginComponent },
+  {path:'signup', component :SignupComponent}
+]
 
 @NgModule({
   declarations: [
+    RegisterComponent,
     LoginComponent,
     SignupComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes) // routes for register module
   ]
 })
 export class RegisterModule { }
