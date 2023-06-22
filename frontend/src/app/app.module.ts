913fc915fa4d458df0a0ad9077ffdad56bd3479e
m-saleh96 <scyllies@gmail.com>
2023-06-22 15:23:48 +0300

    update register service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -14,6 +14,7 @@ import { RegisterModule } from './register/register.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     RegisterModule
   ],
   providers: [],
--- frontend/src/app/services/register.service.ts
+++ frontend/src/app/services/register.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class RegisterService {
+@Injectable()
+export class RegisterService{
 
-  constructor() { }
+  // const part from api
+  private apiUrl : string = environment.apiUrl
+
+  constructor(private http:HttpClient) { }
+
+  signUp(registerFormValue:any):Observable<any>
+  {
+    return this.http.post(`${this.apiUrl}/register` , registerFormValue)
+  }
+
 }
