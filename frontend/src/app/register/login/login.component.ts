a3f1310b62bdba4458a0997d1704d73dbb9719cd
m-saleh96 <scyllies@gmail.com>
2023-06-22 22:53:56 +0300

    create cookie service to store userData

--- frontend/src/app/register/login/login.component.ts
+++ frontend/src/app/register/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormGroup , FormControl , Validators} from '@angular/forms';
 import { RegisterService } from 'src/app/services/register.service';
 import { Router } from '@angular/router';
-
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +10,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private registerService:RegisterService , private router:Router){}
+  userData!:any;
+  constructor(private registerService:RegisterService , private router:Router , private cookieService: CookieService){}
   errorMessage:any='';
   flag:boolean =false;
   loginForm:FormGroup = new FormGroup({
@@ -21,9 +22,13 @@ export class LoginComponent {
   getLoginInfo(loginForm:any)
   {
     if(loginForm.valid == true){
-      this.registerService.login(loginForm.value).subscribe((data)=>{
+      this.registerService.login(loginForm.value).subscribe((data:any)=>{
         if (data) {
-          console.log(data);
+          console.log(data.expires_in);
+        this.cookieService.set('userData', JSON.stringify(data) , data.expires_in ,'Strict');
+        this.userData = JSON.parse(this.cookieService.get('userData') || '{}');
+        console.log(this.userData);
+
         }
         else{
           this.flag = true;
