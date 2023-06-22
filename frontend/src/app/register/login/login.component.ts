c919f41f6cc80c57d12ef7b276d937aa5a311c24
m-saleh96 <scyllies@gmail.com>
2023-06-22 17:17:59 +0300

    in service register create fun login , login component

--- frontend/src/app/register/login/login.component.ts
+++ frontend/src/app/register/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup , FormControl , Validators} from '@angular/forms';
+import { RegisterService } from 'src/app/services/register.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private registerService:RegisterService , private router:Router){}
+  errorMessage:any='';
+  flag:boolean =false;
+  loginForm:FormGroup = new FormGroup({
+    'email':new FormControl(null , [Validators.email , Validators.required]),
+    'password':new FormControl(null , [Validators.required])
+  });
 
+  getLoginInfo(loginForm:any)
+  {
+    if(loginForm.valid == true){
+      this.registerService.login(loginForm.value).subscribe((data)=>{
+        if (data ) {
+          console.log(data);
+
+        }
+        else{
+          this.flag = true;
+          this.errorMessage = data;
+        }
+        // console.log(data)
+
+      })
+    }else{
+      this.flag = true;
+    }
+  }
 }
