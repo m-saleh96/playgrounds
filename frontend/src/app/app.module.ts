810f28b369f6d0f4ebfe2ca9d01cf9eb09139743
m-saleh96 <scyllies@gmail.com>
2023-06-22 14:25:39 +0300

    remove component header and home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RegisterModule } from './register/register.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HeaderComponent,
     NavbarComponent,
   ],
   imports: [
