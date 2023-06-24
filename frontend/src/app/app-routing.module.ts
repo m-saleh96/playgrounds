9bd8dd15ec6a5dd2e6091e133599acbd68118fa8
m-saleh96 <scyllies@gmail.com>
2023-06-24 15:42:43 +0300

    static playgrounds module contain aside for filteration and all feilds

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PlaygroundsComponent } from './playgrounds/playgrounds.component';
 
 const routes: Routes = [
     // lazy loading
     { path:'register', loadChildren: () => import('./register/register.module').then((m) => m.RegisterModule)},
     { path:'home', component: HomeComponent},
+    { path:'playgrounds', component: PlaygroundsComponent},
 ];
 
 @NgModule({
