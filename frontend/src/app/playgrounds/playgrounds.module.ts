79e03257f478a8920579e636fe7c08ca675dab43
m-saleh96 <scyllies@gmail.com>
2023-06-28 04:51:20 +0300

    remove field details component

--- frontend/src/app/playgrounds/playgrounds.module.ts
+++ frontend/src/app/playgrounds/playgrounds.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FieldComponent } from './field/field.component';
 import { PlaygroundsComponent } from './playgrounds.component';
-import { FieldDetailsComponent } from './field-details/field-details.component';
 import { FormsModule } from '@angular/forms';
 import { AsideComponent } from './aside/aside.component';
 import { FilterPlayGroundsService } from '../services/filter-play-grounds.service';
@@ -14,7 +13,6 @@ import { FilterPlayGroundsService } from '../services/filter-play-grounds.servic
   declarations: [
     PlaygroundsComponent,
     FieldComponent,
-    FieldDetailsComponent,
     AsideComponent,
   ],
   imports: [
