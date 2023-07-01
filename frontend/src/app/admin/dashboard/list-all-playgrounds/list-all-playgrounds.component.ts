2de5f330fef760ecddeeed9d42e37905bd6a9e70
HendAdel1 <1hend.adel1@gmail.com>
2023-07-01 20:29:59 +0300

    admin list-all-playgrounds created

---
+++ frontend/src/app/admin/dashboard/list-all-playgrounds/list-all-playgrounds.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-all-playgrounds',
+  templateUrl: './list-all-playgrounds.component.html',
+  styleUrls: ['./list-all-playgrounds.component.css']
+})
+export class ListAllPlaygroundsComponent {
+
+}
