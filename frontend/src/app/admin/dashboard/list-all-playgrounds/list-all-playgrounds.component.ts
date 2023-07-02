9f86d84135ce97ade01bebbda6d00080ed3d70b5
HendAdel1 <1hend.adel1@gmail.com>
2023-07-02 20:04:32 +0300

    remove map from button

--- frontend/src/app/admin/dashboard/list-all-playgrounds/list-all-playgrounds.component.ts
+++ frontend/src/app/admin/dashboard/list-all-playgrounds/list-all-playgrounds.component.ts
@@ -13,13 +13,11 @@ export class ListAllPlaygroundsComponent {
 
   playgrounds!: Playground []
   accessToken!: string;
-  statusUpdatedMap = new Map<number, boolean>();
   faCheck = faCheck;
 
   constructor(private playgroundService: PlaygroundService, private cookieService: CookieService) { }
 
   ngOnInit(): void {
-    // this.accessToken = this.cookieService.get('userData').access_token;
     this.accessToken = JSON.parse(this.cookieService.get('userData') || '{}').access_token;
     this.playgroundService.listPending(this.accessToken).subscribe((res: any) => this.playgrounds = res);
 
@@ -29,7 +27,6 @@ export class ListAllPlaygroundsComponent {
       // playground status updated
     this.playgroundService.updateStatus(id, 'done', this.accessToken).subscribe((res) => {
       console.log(res);
-      this.statusUpdatedMap.set(id, true); // set statusUpdated for selected playground
 
       // reload playgrounds list
       this.playgroundService.listPending(this.accessToken).subscribe((res: any) => this.playgrounds = res);
