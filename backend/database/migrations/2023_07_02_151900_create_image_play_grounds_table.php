4a1a04899309866a22d8e360e56e2eda4a3069e6
gerges2 <gergesvictor512@gmail.com>
2023-07-02 21:19:23 +0300

    update review controler

---
+++ backend/database/migrations/2023_07_02_151900_create_image_play_grounds_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('image_play_grounds', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('playgrounds_id')->constrained('playgrounds')->onDelete('cascade');
+            $table->string('subImage');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('image_play_grounds');
+    }
+};
