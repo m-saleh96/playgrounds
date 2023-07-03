7b9f9f2640bd82bc1cd03fa029d3de8ac236c847
HendAdel1 <1hend.adel1@gmail.com>
2023-07-03 23:39:31 +0300

    redesign top rated cards

--- frontend/src/app/home/top-rated-playgrounds/top-rated-playgrounds.component.ts
+++ frontend/src/app/home/top-rated-playgrounds/top-rated-playgrounds.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Playground } from 'src/app/interfaces/playground';
 import { PlaygroundService } from 'src/app/services/playground.service';
 
 @Component({
@@ -8,15 +10,21 @@ import { PlaygroundService } from 'src/app/services/playground.service';
 })
 export class TopRatedPlaygroundsComponent implements OnInit {
   topRatedPlaygrounds!: any[];
+  @Input() playground !: Playground
 
-  constructor(private playgroundService: PlaygroundService) {}
+  constructor(private playgroundService: PlaygroundService, private router:Router) {}
 
   ngOnInit(): void {
     this.getTopRatedPlaygrounds();
+    this.topRatedPlaygrounds.splice(3);
   }
 
   getTopRatedPlaygrounds(): void {
     this.playgroundService.getTopRatedPlaygrounds()
       .subscribe(playgrounds => this.topRatedPlaygrounds = playgrounds);
+  }
+
+  redirectToDetails(id: number){
+    this.router.navigate(['playground/details/',id])
   }
 }
