b4a581829dccbc2ed4bcbc6685eb34ada822823b
HendAdel1 <1hend.adel1@gmail.com>
2023-07-03 22:44:06 +0300

    get top-rated-playgrounds from back and display in front 'add needed code to it's html, ts and playground service

--- frontend/src/app/home/top-rated-playgrounds/top-rated-playgrounds.component.ts
+++ frontend/src/app/home/top-rated-playgrounds/top-rated-playgrounds.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlaygroundService } from 'src/app/services/playground.service';
 
 @Component({
   selector: 'app-top-rated-playgrounds',
   templateUrl: './top-rated-playgrounds.component.html',
   styleUrls: ['./top-rated-playgrounds.component.css']
 })
-export class TopRatedPlaygroundsComponent {
+export class TopRatedPlaygroundsComponent implements OnInit {
+  topRatedPlaygrounds!: any[];
 
+  constructor(private playgroundService: PlaygroundService) {}
+
+  ngOnInit(): void {
+    this.getTopRatedPlaygrounds();
+  }
+
+  getTopRatedPlaygrounds(): void {
+    this.playgroundService.getTopRatedPlaygrounds()
+      .subscribe(playgrounds => this.topRatedPlaygrounds = playgrounds);
+  }
 }
