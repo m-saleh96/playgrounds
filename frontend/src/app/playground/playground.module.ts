c1051bc0ea7a92916a229dffeeb12a66eec31e74
HendAdel1 <1hend.adel1@gmail.com>
2023-07-06 00:07:19 +0300

    solve error

--- frontend/src/app/playground/playground.module.ts
+++ frontend/src/app/playground/playground.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListComponent } from './list/list.component';
 import { CardComponent } from './card/card.component';
 import { DetailsComponent } from './details/details.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -10,14 +9,12 @@ import { PlaygroundComponent } from './playground.component';
 
 // routes for playground module
 const routes: Routes = [
-  {path:'list', component: ListComponent },
   {path:'details/:id', component : DetailsComponent},
   {path:'card', component : CardComponent},
 ]
 @NgModule({
   declarations: [
     PlaygroundComponent,
-    ListComponent,
     CardComponent,
     DetailsComponent,
   ],
