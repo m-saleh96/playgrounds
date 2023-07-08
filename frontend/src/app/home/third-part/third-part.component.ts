826255901b31959577674c94cefed4b68a908202
semon saleeb <semonsaleeb@gmail.com>
2023-07-08 17:09:51 +0200

    add review details page

--- frontend/src/app/home/third-part/third-part.component.ts
+++ frontend/src/app/home/third-part/third-part.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-third-part',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class ThirdPartComponent {
 
-}
+  constructor(private router:Router){}
+
+
+goToRegesterPage(){
+    this.router.navigate(['signup'])
+  }
+ }
