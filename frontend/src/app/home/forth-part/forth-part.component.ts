8837b0d65652a56776b742dc3803dc074c8a5fba
HendAdel1 <1hend.adel1@gmail.com>
2023-07-09 19:05:41 +0300

    forth-part gome generated

---
+++ frontend/src/app/home/forth-part/forth-part.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forth-part',
+  templateUrl: './forth-part.component.html',
+  styleUrls: ['./forth-part.component.css']
+})
+export class ForthPartComponent {
+
+}
